a06c1c5e69ec2c6ffd80b8df99de9705038857bd
Orlicz <2328279958@qq.com>
2022-07-08 16:44:27 +0800

    qwqww

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,3 @@ export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-// my add
-function getDom(value){
-  var selectDom = [];
-  var dom=document.getElementsByClassName("hidden md:block")[0].getElementsByTagName('button');
-  for (var i=0; i<dom.length; i++) {
-      if(value===dom[i].innerText){
-          selectDom.push(dom[i]);
-      }
-  }
-  return selectDom;
-}
-getDom('默认')[0].click()
