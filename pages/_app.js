430feff4d42270130ade1597fae99ca2c6f77a4b
Orlicz <2328279958@qq.com>
2022-07-08 16:39:16 +0800

    qwqw

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,16 @@ import 'nextra-theme-docs/style.css'
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+// my add
+function getDom(value){
+  var selectDom = [];
+  var dom=document.getElementsByClassName("hidden md:block")[0].getElementsByTagName('button');
+  for (var i=0; i<dom.length; i++) {
+      if(value===dom[i].innerText){
+          selectDom.push(dom[i]);
+      }
+  }
+  return selectDom;
+}
+getDom('默认')[0].click()
